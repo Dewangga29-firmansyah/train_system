c97406c0e229cf64b5e5eb7356b2a91eba7b172e
MohammadKevin <kvn4.200581@gmail.com>
2026-06-02 02:42:02 +0700

    last

--- src/pembelian/pembelian.controller.ts
+++ src/pembelian/pembelian.controller.ts
@@ -8,63 +8,95 @@ import {
   Req,
   Patch,
   Res,
-} from '@nestjs/common';
-import { PembelianService } from './pembelian.service';
-import { JwtAuthGuard } from 'src/auth/guard/jwt-auth.guard';
-import { RolesGuard } from 'src/auth/guard/roles.guard';
-import { Roles } from 'src/auth/decorators/roles.decorator';
-import { CreatePembelianDto } from './dto/create-pembelian.dto';
-import express from 'express';
+} from '@nestjs/common'
+
+import { Response } from 'express'
+
+import { PembelianService } from './pembelian.service'
+
+import { JwtAuthGuard } from 'src/auth/guard/jwt-auth.guard'
+import { RolesGuard } from 'src/auth/guard/roles.guard'
+import { Roles } from 'src/auth/decorators/roles.decorator'
+
+import { CreatePembelianDto } from './dto/create-pembelian.dto'
 
 @Controller('pembelian')
 export class PembelianController {
-  constructor(private readonly pembelianService: PembelianService) {}
+  constructor(
+    private readonly pembelianService: PembelianService,
+  ) {}
 
   @Post()
   @UseGuards(JwtAuthGuard)
-  create(@Req() req: any, @Body() dto: CreatePembelianDto) {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-member-access
-    return this.pembelianService.create(req.user.sub, dto);
+  create(
+    @Req() req: any,
+    @Body() dto: CreatePembelianDto,
+  ) {
+    return this.pembelianService.create(
+      req.user.sub,
+      dto,
+    )
+  }
+
+  @Get('mine')
+  @UseGuards(JwtAuthGuard)
+  findMine(
+    @Req() req: any,
+  ) {
+    return this.pembelianService.findMine(
+      req.user.sub,
+    )
   }
 
   @Patch(':id/confirm')
   @UseGuards(JwtAuthGuard)
-  confirm(@Param('id') id: string) {
-    return this.pembelianService.confirmPayment(id);
+  confirm(
+    @Param('id') id: string,
+  ) {
+    return this.pembelianService.confirmPayment(
+      id,
+    )
   }
 
   @Patch(':id/cancel')
   @UseGuards(JwtAuthGuard)
-  cancel(@Param('id') id: string) {
-    return this.pembelianService.cancelPembelian(id);
+  cancel(
+    @Param('id') id: string,
+  ) {
+    return this.pembelianService.cancelPembelian(
+      id,
+    )
   }
 
   @Get(':id/tiket')
   @UseGuards(JwtAuthGuard)
-  generateTiket(@Param('id') id: string, @Res() res: express.Response) {
-    return this.pembelianService.generateTiketPdf(id, res);
+  tiket(
+    @Param('id') id: string,
+    @Res() res: Response,
+  ) {
+    return this.pembelianService.generateTiketPdf(
+      id,
+      res,
+    )
   }
 
   @Get()
-  @UseGuards(JwtAuthGuard, RolesGuard)
+  @UseGuards(
+    JwtAuthGuard,
+    RolesGuard,
+  )
   @Roles('ADMIN')
   findAll() {
-    return this.pembelianService.findAll();
+    return this.pembelianService.findAll()
   }
 
   @Get(':id')
   @UseGuards(JwtAuthGuard)
-  findOne(@Param('id') id: string) {
-    return this.pembelianService.findOne(id);
+  findOne(
+    @Param('id') id: string,
+  ) {
+    return this.pembelianService.findOne(
+      id,
+    )
   }
-
-  @Get('mine')
-@UseGuards(JwtAuthGuard)
-findMine(
-  @Req() req: any,
-) {
-  return this.pembelianService.findMine(
-    req.user.sub,
-  )
-}
 }
