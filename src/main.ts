13b3cbf08095337ffeb3a24fa76242006493337a
MohammadKevin <kvn4.200581@gmail.com>
2026-06-03 07:04:54 +0700

    backend selesai

--- src/main.ts
+++ src/main.ts
@@ -7,17 +7,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: [
-      'http://localhost:3000',
-      'http://127.0.0.1:3000',
-    ],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    allowedHeaders: [
-      'Content-Type',
-      'Authorization',
-    ],
+    origin: true,
     credentials: true,
-  });
+  })
 
   app.useGlobalPipes(
     new ValidationPipe({
