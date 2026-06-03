72f1a5611554424e61709fecaa9b2d90da84b283
MohammadKevin <kvn4.200581@gmail.com>
2026-06-03 07:27:56 +0700

    cors final

--- src/main.ts
+++ src/main.ts
@@ -1,76 +1,57 @@
-import { NestFactory } from '@nestjs/core'
-import { AppModule } from './app.module'
-import { ValidationPipe } from '@nestjs/common'
-import {
-  SwaggerModule,
-  DocumentBuilder,
-} from '@nestjs/swagger'
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app =
     await NestFactory.create(
       AppModule,
-    )
+    );
 
   app.enableCors({
-    origin: [
-      'http://localhost:3000',
-    ],
-
-    methods: [
-      'GET',
-      'POST',
-      'PUT',
-      'PATCH',
-      'DELETE',
-      'OPTIONS',
-    ],
-
-    allowedHeaders: [
-      'Content-Type',
-      'Authorization',
-      'Accept',
-      'Origin',
-    ],
-
-    credentials: false,
-  })
+    origin: '*',
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       transform: true,
     }),
-  )
+  );
 
   const config =
     new DocumentBuilder()
       .setTitle('Train API')
-      .setDescription('Train API Docs')
+      .setDescription(
+        'Train API Docs',
+      )
       .setVersion('1.0')
       .addBearerAuth()
-      .build()
+      .build();
 
   const document =
     SwaggerModule.createDocument(
       app,
       config,
-    )
+    );
 
   SwaggerModule.setup(
     'api',
     app,
     document,
-  )
+  );
 
   await app.listen(
-    process.env.PORT || 3000,
-    '0.0.0.0', // penting di Railway
-  )
+    Number(
+      process.env.PORT
+    ) || 3000,
+    '0.0.0.0'
+  );
 
   console.log(
-    `RUNNING`
-  )
+    'RUNNING'
+  );
 }
 
-bootstrap()
+bootstrap();
